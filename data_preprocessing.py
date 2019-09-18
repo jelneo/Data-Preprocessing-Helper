96532b560e263f65d0b7abf1e957f9707982f0f9
jelneo <neohuilingjelena@gmail.com>
2019-09-19 00:06:49 +0800

    Combine processing of VV and VH bands

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,34 +8,36 @@ from snappy import GPF
 
 import snappyconfigs
 
-AOI_WKT = "POLYGON ((102.12635428857155 14.248290232074497, 102.53781291893677 14.327232114630814, 102.48940057780496 14.575151504929456, 102.0774759551546 14.496314998624996, 102.12635428857155 14.248290232074497))"
-
-polarizations = ["VV"]
+AOI_WKT = \
+"POLYGON((102.070752316744 14.565580568454624,102.36824148300377 14.565580568454624,102.36824148300377 14.322211910367955,102.070752316744 14.322211910367955,102.070752316744 14.565580568454624))"
+polarizations = "VV,VH"
 
 GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
 
 usr_system = platform.system()
 if usr_system == "Windows":
     parent_dir = "D:\\ESA Tutorial\\"
-    # input_dir = parent_dir + "Original\\"
-    # output_dir = parent_dir + "Processing\\"
-    input_dir = parent_dir + "Test\\"
-    output_dir = parent_dir + "TestProc\\"
+    input_dir = parent_dir + "Original\\"
+    output_dir = parent_dir + "Processing\\"
+    # input_dir = parent_dir + "Test\\"
+    # output_dir = parent_dir + "TestProc\\"
+    manifest_extension = "\\manifest.safe"
 elif usr_system == "Linux":
     # For hpc
-    parent_dir = ".\\"
-    input_dir = parent_dir + "Original\\"
-    output_dir = input_dir + "Processing\\"
+    parent_dir = "/hpctmp2/a0158174/"
+    input_dir = parent_dir + "Original/"
+    output_dir = input_dir + "Processing/"
+    manifest_extension = "/manifest.safe"
 else:
     print("Unrecognized system, no directory set.\nProgram exiting...")
     exit(1)
 
 toRun = input("Run program? (Y/N)")
 if toRun == "Y" or toRun == "y":
-    exit(0)
     pass
 else:
     print("Exiting...")
+    exit(0)
 
 gc.enable()
 now = datetime.datetime.now()
@@ -60,10 +62,11 @@ preprocessing order depends on product type(slc,grnd,raw) and product level(0,1,
 # (make sure that the data is already unzipped)
 for folder in os.listdir(input_dir):
 
-    if ".SAFE" not in folder:
-        continue
-    # if "S1B_IW_GRDH_1SDV_20190829T112005_20190829T112030_017800_0217F9_F111.SAFE" != folder:
+    # if ".SAFE" not in folder:
     #     continue
+    if "S1A_IW_GRDH_1SDV_20190324T230053_20190324T230118_026486_02F776_6EB6.SAFE" != folder \
+            and "S1B_IW_GRDH_1SDV_20190821T230023_20190821T230048_017690_021483_098C.SAFE" != folder:
+        continue
 
     input_file_name = input_dir + folder
     file_name = folder[:-5]
@@ -72,60 +75,52 @@ for folder in os.listdir(input_dir):
 
     print("Current folder: " + folder)
     # Read in the Sentinel-1 data product:
-    sentinel_1 = ProductIO.readProduct(input_file_name + "\\manifest.safe")
+    sentinel_1 = ProductIO.readProduct(input_file_name + manifest_extension)
     print(sentinel_1)
 
-    # If polarization bands are available, split up your code to process VH and VV intensity data separately.
-    # The first step is the calibration procedure by transforming the DN values to Sigma Naught respectively.
-    # You can specify the parameters to input_file_name the Image in Decibels as well.
+    ### THERMAL NOISE REMOVAL
+    parameters = snappyconfigs.get_thermal_noise_removal_config()
+    noise_removal_path = output_dir + file_name + "_" + "_noise_removal"
+    noise_rem_prdt = GPF.createProduct("ThermalNoiseRemoval", parameters, sentinel_1)
+    ProductIO.writeProduct(noise_rem_prdt, noise_removal_path, "BEAM-DIMAP")
+    print("Thermal noise removal done")
 
-    for p in polarizations:
-        polarization = p
-        ### THERMAL NOISE REMOVAL
-        parameters = snappyconfigs.get_thermal_noise_removal_config()
-        noise_removal_path = output_dir + file_name + "_" + date + "_noise_removal_" + polarization
-        noise_rem_prdt = GPF.createProduct("ThermalNoiseRemoval", parameters, sentinel_1)
-        ProductIO.writeProduct(noise_rem_prdt, noise_removal_path, "BEAM-DIMAP")
-        print("Thermal noise removal done")
+    ### APPLY-ORBIT FILE
+    parameters = snappyconfigs.get_thermal_noise_removal_config()
+    apply_orbit_path = output_dir + file_name + "_" + "_orbit"
+    apply_orbit_prdt = GPF.createProduct("Apply-Orbit-File", parameters, noise_rem_prdt)
+    ProductIO.writeProduct(apply_orbit_prdt, apply_orbit_path, "BEAM-DIMAP")
+    print("Apply-orbit file done")
 
-        ### APPLY-ORBIT FILE
-        parameters = snappyconfigs.get_thermal_noise_removal_config()
-        apply_orbit_path = output_dir + file_name + "_" + date + "_orbit_" + polarization
-        apply_orbit_prdt = GPF.createProduct("Apply-Orbit-File", parameters, noise_rem_prdt)
-        ProductIO.writeProduct(apply_orbit_prdt, apply_orbit_path, "BEAM-DIMAP")
-        print("Apply-orbit file done")
+    ### CALIBRATION
+    parameters = snappyconfigs.get_calibration_config(polarizations)
+    calib_path = output_dir + file_name + "_" + "_calibrate"
+    calibrated_prdt = GPF.createProduct("Calibration", parameters, apply_orbit_prdt)
+    ProductIO.writeProduct(calibrated_prdt, calib_path, "BEAM-DIMAP")
+    print("Calibration done")
 
-        ### CALIBRATION
-        parameters = snappyconfigs.get_calibration_config(polarization)
-        calib_path = output_dir + file_name + "_" + date + "_calibrate_" + polarization
-        calibrated_prdt = GPF.createProduct("Calibration", parameters, apply_orbit_prdt)
-        ProductIO.writeProduct(calibrated_prdt, calib_path, "BEAM-DIMAP")
-        print("Calibration done")
+    ### SUBSET
+    parameters = snappyconfigs.get_subset_config(AOI_WKT)
+    subset_path = output_dir + file_name + "_" + "_subset"
+    subset_prdt = GPF.createProduct("Subset", parameters, calibrated_prdt)
+    ProductIO.writeProduct(subset_prdt, subset_path, "BEAM-DIMAP")
+    print("Subset done")
 
-        ### SUBSET
-        parameters = snappyconfigs.get_subset_config(AOI_WKT)
-        subset_path = output_dir + file_name + "_" + date + "_subset_" + polarization
-        subset_prdt = GPF.createProduct("Subset", parameters, calibrated_prdt)
-        ProductIO.writeProduct(subset_prdt, subset_path, "BEAM-DIMAP")
-        print("Subset done")
+    ### SPECKLE FILTER
+    parameters = snappyconfigs.get_speckle_filter_config()
+    speckle_path = output_dir + file_name + "_" + "_speckle"
+    speckle_prdt = GPF.createProduct("Speckle-Filter", parameters, subset_prdt)
+    ProductIO.writeProduct(speckle_prdt, speckle_path, "BEAM-DIMAP")
+    print("Speckle filter done")
 
-        ### SPECKLE FILTER
-        parameters = snappyconfigs.get_speckle_filter_config()
-        speckle_path = output_dir + file_name + "_" + date + "_speckle_" + polarization
-        speckle_prdt = GPF.createProduct("Speckle-Filter", parameters, subset_prdt)
-        ProductIO.writeProduct(speckle_prdt, speckle_path, "BEAM-DIMAP")
-        print("Speckle filter done")
-
-        ### TERRAIN CORRECTION
-        parameters = snappyconfigs.get_terrain_correction_config()
-        terrain = output_dir + file_name + "_" + date + "_corrected_" + polarization
-        terrain_corrected_prdt = GPF.createProduct("Terrain-Correction", parameters, speckle_prdt)
-        ProductIO.writeProduct(terrain_corrected_prdt, terrain, "GeoTIFF")
-        # ProductIO.writeProduct(terrain_corrected_prdt, terrain + "_big", "GeoTIFF-BigTIFF")
-        print("Terrain correction done")
-
-        print("Done with " + polarization + " for " + folder)
-        print()
+    ### TERRAIN CORRECTION
+    parameters = snappyconfigs.get_terrain_correction_config()
+    terrain = output_dir + file_name + "_" + "_corrected"
+    terrain_corrected_prdt = GPF.createProduct("Terrain-Correction", parameters, speckle_prdt)
+    ProductIO.writeProduct(terrain_corrected_prdt, terrain, "GeoTIFF")
+    # ProductIO.writeProduct(terrain_corrected_prdt, terrain + "_big", "GeoTIFF-BigTIFF")
+    print("Terrain correction done")
+    print()
 
 now = datetime.datetime.now()
 print("Finished at " + now.strftime("%Y-%m-%d %H:%M:%S"))
