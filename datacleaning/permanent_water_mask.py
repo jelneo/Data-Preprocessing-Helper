78ef5b5c62abaabdd9220056ec9f73a442b502ce
jelneo <neohuilingjelena@gmail.com>
2019-10-18 00:07:36 +0800

    Set no-data region and extract WKT

--- datacleaning/permanent_water_mask.py
+++ datacleaning/permanent_water_mask.py
@@ -6,6 +6,8 @@ import rasterio
 import numpy as np
 import logging
 import matplotlib.pyplot as plt
+from rasterio.features import shapes
+from shapely.geometry import shape
 
 BLACK = 0
 WHITE = 255
@@ -90,6 +92,7 @@ for folder in os.listdir(parent_dir):
     if folder.endswith(".tif") and 'land_water_mask' in folder:
         logger.info("opened")
         img = rasterio.open(parent_dir + folder)
+        print(img.crs)
         msk = Image.open(parent_dir + folder)
         width = img.width
         height = img.height
@@ -109,14 +112,44 @@ for folder in os.listdir(parent_dir):
                         break
             if done:
                 break
-        msk.show()
+        # msk.show()
         msk_arr = np.array(msk)
-        msk_arr[msk_arr == BLACK] = WHITE
-        msk_arr[msk_arr == DAM_COLOR] = BLACK
-        msk = Image.fromarray(msk_arr)
-        msk.show()
+        # msk_arr[msk_arr == BLACK] = BLACK
+        msk_arr[msk_arr == WHITE] = BLACK
+        # msk_arr[msk_arr == DAM_COLOR] = BLACK
+
+        # msk = Image.fromarray(msk_arr)
+        # msk.show()
         # plt.imshow(p_img_arr)
         # plt.show()
         file_name = re.sub("\\..*$", "", folder)
-        msk.save(mask_dir + file_name[:-7] + '.tif')
+        mask_prdt_path = mask_dir + file_name[:-7] + '.tif'
+        with rasterio.open(
+            mask_prdt_path,
+            'w',
+            driver='GTiff',
+            height=img.height,
+            width=img.width,
+            count=1,
+            dtype=np.uint8,
+            nodata=BLACK,
+            crs=img.crs,
+            transform=img.transform,
+        ) as dst:
+            dst.write(msk_arr, 1)
+        # msk.save(mask_dir + file_name[:-7] + '.tif')
+
+        mask = None
+        with rasterio.open(mask_prdt_path, 'r') as src:
+            image = src.read(1, masked=True)  # first band
+            print(image)
+            results = (
+                {'properties': {'raster_val': v}, 'geometry': s}
+                for i, (s, v) in enumerate(shapes(image, mask=mask, transform=src.transform)))
+        geoms = list(results)
+        shp = shape(geoms[0]['geometry'])
+        print(shp)
+        a, b = shp.exterior.xy
+        plt.plot(a, b)
+        plt.show()
 logger.info("Completed")
