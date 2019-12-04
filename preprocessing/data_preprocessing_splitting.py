771b9cce69d551322c045ccfc33a509454eb6c85
jelneo <neohuilingjelena@gmail.com>
2019-12-04 12:39:01 +0800

    Refactor methods and comments

--- preprocessing/data_preprocessing_splitting.py
+++ preprocessing/data_preprocessing_splitting.py
@@ -1,30 +1,3 @@
-import datetime
-import platform
-
-import os, gc
-
-from snappy import ProductIO
-from snappy import GPF
-
-import filemanager
-from snappy_tools import snappyoperators as sp
-
-polarizations = "VV"
-
-GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
-
-toRun = input("Run program? (Y/N)")
-if toRun == "Y" or toRun == "y":
-    pass
-else:
-    print("Exiting...")
-    exit(0)
-
-gc.enable()
-now = datetime.datetime.now()
-print("Start data preprocessing at " + now.strftime("%Y-%m-%d %H:%M:%S"))
-print()
-
 """
 Does the following preprocessing steps for chosen prdt type (IW SLC level 1 prdt):
 
@@ -36,44 +9,76 @@ To reduce the area to AOI:
 
 * Note: Cannot subset or deburst before doing back-geocoding
 * Subset always the last step else the areas cropped out will not be the same due to processing e.g. terrain correction
-Preprocessing order depends on product type(slc, grd, raw) and product level(0,1,2) 
+Preprocessing order depends on product type(slc, grd, raw) and product level(0,1,2)
 """
 
-input_dir, output_dir = filemanager.get_file_paths_based_on_os(platform.system())
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+import gc
+import os
+import platform
+import re
+
+from snappy import GPF
+from snappy import ProductIO
+
+import filemanager
+from snappy_tools import snappyoperators as sp
+from basicconfig import POLARIZATIONS
+
+GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+
+toRun = input("Run program? (Y/N)")
+if toRun == "Y" or toRun == "y":
+    pass
+else:
+    print("Exiting...")
+    exit(0)
+
+gc.enable()
+
+logger.info("Start data preprocessing for top sar splitting")
+
+input_dir, output_dir = filemanager.get_file_paths_based_on_os(platform.system(), filemanager.Product.slc)
 for folder in os.listdir(input_dir):
     if ".SAFE" not in folder:
         continue
-    # if "S1A_IW_GRDH_1SDV_20190324T230053_20190324T230118_026486_02F776_6EB6.SAFE" != folder \
-    #         and "S1B_IW_GRDH_1SDV_20190821T230023_20190821T230048_017690_021483_098C.SAFE" != folder:
+    # if "S1B_IW_SLC__1SDV_20190101T111958_20190101T112025_014300_01A9AA_5FE6.SAFE" != folder \
+    #         and "S1A_IW_SLC__1SDV_20190119T112033_20190119T112100_025546_02D56B_2E7C.SAFE" != folder:
     #     continue
+    if 'S1B_IW_SLC__1SDV_20190206T111957_20190206T112024_014825_01BAA2_194F.SAFE' != folder:
+        continue
 
     input_file_name = input_dir + folder
-    file_name = folder[:-5]
+    file_name = re.sub("\\..*$", "", folder)
     timestamp = folder.split("_")[4]
     date = timestamp[:8]
 
-    print("Current folder: " + folder)
+    logger.info('Current folder: ' + folder)
     # Read in the Sentinel-1 data product:
     sentinel_1 = ProductIO.readProduct(input_file_name + filemanager.manifest_extension)
-    print(sentinel_1)
+    logger.info(sentinel_1)
 
     # TOPSAR SPLIT
-    if "S1B" in input_file_name:
-        split_prdt = sp.top_sar_split(sentinel_1, "IW2", 6, 8, polarizations)
+    if 'S1B' in input_file_name:
+        split_prdt = sp.top_sar_split(sentinel_1, 'IW2', 5, 6, POLARIZATIONS)
     else:
-        split_prdt = sp.top_sar_split(sentinel_1, "IW2", 8, 9, polarizations)
+        split_prdt = sp.top_sar_split(sentinel_1, 'IW2', 7, 8, POLARIZATIONS)
 
     # APPLY-ORBIT FILE
     apply_orbit_prdt = sp.apply_orbit_file(split_prdt)
-    apply_orbit_path = output_dir + file_name + "_orbit"
-    ProductIO.writeProduct(apply_orbit_prdt, apply_orbit_path, "BEAM-DIMAP")
 
+    split_path = output_dir + file_name + f'_top_sar_split_{POLARIZATIONS}'
+    ProductIO.writeProduct(apply_orbit_prdt, split_path, 'BEAM-DIMAP')
+    logger.info('Write done')
 
-now = datetime.datetime.now()
-print("Finished at " + now.strftime("%Y-%m-%d %H:%M:%S"))
+logger.info("Completed")
 if platform.system() == "Windows":
     import winsound
-
     duration = 1000  # milliseconds
     freq = 1000  # Hz
     winsound.Beep(freq, duration)
