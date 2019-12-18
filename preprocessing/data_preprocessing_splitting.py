2f3cdb815b852b9108edabc19d148222f78aaa50
jelneo <neohuilingjelena@gmail.com>
2019-12-18 11:52:31 +0800

    Perform TOP SAR Split for multiple SLC products

--- preprocessing/data_preprocessing_splitting.py
+++ preprocessing/data_preprocessing_splitting.py
@@ -28,7 +28,7 @@ from snappy import ProductIO
 
 import filemanager
 from snappy_tools import snappyoperators as sp
-from basicconfig import POLARIZATIONS
+from basicconfig import POLARIZATIONS, MANIFEST_EXTENSION
 
 GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
 
@@ -44,23 +44,31 @@ gc.enable()
 logger.info("Start data preprocessing for top sar splitting")
 
 input_dir, output_dir = filemanager.get_file_paths_based_on_os(platform.system(), filemanager.Product.slc)
+# input_dir = "E://SLC//Original//"
+# due to java heap memory space limits, only able to process 15 products at a time
+count = 15
+existing_files = [f[:-21] for f in os.listdir(output_dir) if '.dim' in f and 'top_sar' in f]
+
 for folder in os.listdir(input_dir):
     if ".SAFE" not in folder:
         continue
     # if "S1B_IW_SLC__1SDV_20190101T111958_20190101T112025_014300_01A9AA_5FE6.SAFE" != folder \
     #         and "S1A_IW_SLC__1SDV_20190119T112033_20190119T112100_025546_02D56B_2E7C.SAFE" != folder:
     #     continue
-    if 'S1B_IW_SLC__1SDV_20190206T111957_20190206T112024_014825_01BAA2_194F.SAFE' != folder:
-        continue
+    # if 'S1B_IW_SLC__1SDV_20190206T111957_20190206T112024_014825_01BAA2_194F.SAFE' != folder:
+    #     continue
 
     input_file_name = input_dir + folder
     file_name = re.sub("\\..*$", "", folder)
-    timestamp = folder.split("_")[4]
+    timestamp = folder.split("_")[5]
     date = timestamp[:8]
+
+    if any(file_name in ef for ef in existing_files):
+        continue
 
     logger.info('Current folder: ' + folder)
     # Read in the Sentinel-1 data product:
-    sentinel_1 = ProductIO.readProduct(input_file_name + filemanager.manifest_extension)
+    sentinel_1 = ProductIO.readProduct(input_file_name + MANIFEST_EXTENSION)
     logger.info(sentinel_1)
 
     # TOPSAR SPLIT
@@ -75,6 +83,9 @@ for folder in os.listdir(input_dir):
     split_path = output_dir + file_name + f'_top_sar_split_{POLARIZATIONS}'
     ProductIO.writeProduct(apply_orbit_prdt, split_path, 'BEAM-DIMAP')
     logger.info('Write done')
+    count -= 1
+    if count == 0:
+        break
 
 logger.info("Completed")
 if platform.system() == "Windows":
