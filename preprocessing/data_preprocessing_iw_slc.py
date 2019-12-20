2d91abc4e115614964fd68269512e1f1c8ad6a43
jelneo <neohuilingjelena@gmail.com>
2019-12-20 15:44:24 +0800

    Process multiple pairs of images for interferogram creation

--- preprocessing/data_preprocessing_iw_slc.py
+++ preprocessing/data_preprocessing_iw_slc.py
@@ -55,7 +55,7 @@ from snappy import GPF
 from snappy import ProductIO
 
 from snappy_tools import snappyconfigs, snappyoperators as sp
-from basicconfig import LC_WKT, POLARIZATIONS, SNAPHU_PATH
+from basicconfig import LC_WKT, POLARIZATIONS, SNAPHU_PATH, COMMON_FILES_NAME
 import filemanager
 
 AOI_WKT = \
@@ -65,7 +65,7 @@ AOI_WKT = \
 GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
 
 usr_system = platform.system()
-input_dir, output_dir = filemanager.get_file_paths_based_on_os(platform.system(), filemanager.Product.slc)
+input_dir, output_dir = filemanager.get_file_paths_based_on_os(usr_system, filemanager.Product.slc)
 
 toRun = input("Run program? (Y/N)")
 if toRun == "Y" or toRun == "y":
@@ -107,113 +107,120 @@ def call_snaphu_command(snaphu_path):
         logger.critical("Snaphu error output:\n", e.output)
 
 
-# Testing using one master and one slave pair
-# (make sure that the data is already unzipped)
+existing_files = [f for f in os.listdir(output_dir) if f[0].isdigit()]
+count = 10
+# read list of files from common_file.txt in processing dir (already sorted in chronological order)
+# use normal for-loop
+with open(output_dir + COMMON_FILES_NAME) as f:
+    try:
+        files_to_process = f.readlines()[:-1]
+    except IOError:
+        print("File read error")
 
-# if ".SAFE" not in folder:
-#     continue
-# if "S1A_IW_SLC__1SDV_20190107T112033_20190107T112100_025371_02CF15_1237__subset.tif" != folder \
-#         and "S1B_IW_SLC__1SDV_20190101T111958_20190101T112025_014300_01A9AA_5FE6__subset.tif" != folder:
-#     continue
+for i in range(len(files_to_process) - 1):
+    ######################## COREGISTRATION ########################
+    # master_file = "S1B_IW_SLC__1SDV_20190101T111958_20190101T112025_014300_01A9AA_5FE6.SAFE"
+    # slave_file = "S1A_IW_SLC__1SDV_20190107T112033_20190107T112100_025371_02CF15_1237.SAFE"
 
-######################## COREGISTRATION ########################
-logger.info("Start data preprocessing for iw slc")
-master_file = "S1B_IW_SLC__1SDV_20190101T111958_20190101T112025_014300_01A9AA_5FE6.SAFE"
-slave_file = "S1A_IW_SLC__1SDV_20190107T112033_20190107T112100_025371_02CF15_1237.SAFE"
+    # input_file_name1 = input_dir + master_file
+    master_file = files_to_process[i][:-1]
+    timestamp1 = master_file.split("_")[5]
+    date1 = timestamp1[:8]
 
-input_file_name1 = input_dir + master_file
-file_name1 = re.sub("\\..*$", "", master_file)
-timestamp1 = master_file.split("_")[5]
-date1 = timestamp1[:8]
+    # input_file_name2 = input_dir + slave_file
+    slave_file = files_to_process[i + 1][:-1]
+    timestamp2 = slave_file.split("_")[5]
+    date2 = timestamp2[:8]
 
-input_file_name2 = input_dir + slave_file
-file_name2 = re.sub("\\..*$", "", slave_file)
-timestamp2 = slave_file.split("_")[5]
-date2 = timestamp2[:8]
+    combined_name = timestamp1 + "_" + timestamp2
+    if any(combined_name in ef for ef in existing_files):
+        continue
+    logger.info("Current pair: " + combined_name)
+    # Read in the Sentinel-1 data product:
+    sentinel_1_m = ProductIO.readProduct(output_dir + master_file + f"_top_sar_split_{POLARIZATIONS}.dim")
+    sentinel_1_s = ProductIO.readProduct(output_dir + slave_file + f"_top_sar_split_{POLARIZATIONS}.dim")
+    logger.info("Read")
 
-combined_name = timestamp1 + "_" + timestamp2
+    ### BACK-GEOCODING
+    back_geocoded_prdt = sp.back_geocoding([sentinel_1_s, sentinel_1_m])
 
-# Read in the Sentinel-1 data product:
-sentinel_1_m = ProductIO.readProduct(output_dir + file_name1 + f"_top_sar_split_{POLARIZATIONS}.dim")
-sentinel_1_s = ProductIO.readProduct(output_dir + file_name2 + f"_top_sar_split_{POLARIZATIONS}.dim")
-logger.info("Read")
+    # ### ENHANCED SPECTRAL DIVERSITY
+    esd_prdt = sp.enhanced_spectral_diversity(back_geocoded_prdt)
 
-### BACK-GEOCODING
-back_geocoded_prdt = sp.back_geocoding([sentinel_1_s, sentinel_1_m])
+    ######################## INTERFEROGRAM PROCESSING TO GET DINSAR ########################
+    ### INTERFEROGRAM
+    interferogram_prdt = sp.interferogram(esd_prdt)
 
-# ### ENHANCED SPECTRAL DIVERSITY
-esd_prdt = sp.enhanced_spectral_diversity(back_geocoded_prdt)
+    ### TOPSAR DEBURST
+    deburst_prdt = sp.top_sar_deburst(interferogram_prdt, POLARIZATIONS)
+    deburst_path = output_dir + combined_name + f'_deburst_{POLARIZATIONS}'
+    ProductIO.writeProduct(deburst_prdt, deburst_path, "BEAM-DIMAP")
+    logger.info('Write done')
 
-######################## INTERFEROGRAM PROCESSING TO GET DINSAR ########################
-### INTERFEROGRAM
-interferogram_prdt = sp.interferogram(esd_prdt)
+    ### TOPO PHASE REMOVAL
+    in_deburst_prdt = ProductIO.readProduct(deburst_path + '.dim')
+    tpr_prdt = sp.topo_phase_removal(in_deburst_prdt)
 
-### TOPSAR DEBURST
-deburst_prdt = sp.top_sar_deburst(interferogram_prdt, POLARIZATIONS)
-deburst_path = output_dir + combined_name + f'_deburst_{POLARIZATIONS}'
-ProductIO.writeProduct(deburst_prdt, deburst_path, "BEAM-DIMAP")
-logger.info('Write done')
+    ### MULTILOOK
+    multilook_prdt = sp.multilook(tpr_prdt)
 
-### TOPO PHASE REMOVAL
-in_deburst_prdt = ProductIO.readProduct(deburst_path + '.dim')
-tpr_prdt = sp.topo_phase_removal(in_deburst_prdt)
+    # ### GOLDSTEIN PHASE FILTERING
+    goldstein_prdt = sp.goldstein_phase_filtering(multilook_prdt)
+    goldstein_path = output_dir + combined_name + f'_goldstein_{POLARIZATIONS}'
 
-### MULTILOOK
-multilook_prdt = sp.multilook(tpr_prdt)
+    ProductIO.writeProduct(goldstein_prdt, goldstein_path, "BEAM-DIMAP")
+    logger.info('Write done')
 
-# ### GOLDSTEIN PHASE FILTERING
-goldstein_prdt = sp.goldstein_phase_filtering(multilook_prdt)
-goldstein_path = output_dir + combined_name + f'_goldstein_{POLARIZATIONS}'
+    ####################### PHASE UNWRAPPING ########################
+    ### SNAPHU EXPORT
+    in_goldstein_prdt = ProductIO.readProduct(goldstein_path + '.dim')
+    snaphu_output_path =  output_dir + SNAPHU_PATH + combined_name + f'_{POLARIZATIONS}' + '\\'
+    snaphu_export_prdt = sp.snaphu_export(in_goldstein_prdt, snaphu_output_path)
+    ProductIO.writeProduct(snaphu_export_prdt, snaphu_output_path, "Snaphu")
+    logger.info("Snaphu Export write done")
 
-ProductIO.writeProduct(goldstein_prdt, goldstein_path, "BEAM-DIMAP")
-logger.info('Write done')
+    ### Unwrapping
+    call_snaphu_command(snaphu_output_path)
 
-####################### PHASE UNWRAPPING ########################
-### SNAPHU EXPORT
-in_goldstein_prdt = ProductIO.readProduct(goldstein_path + '.dim')
-snaphu_output_path = SNAPHU_PATH + combined_name + f'_{POLARIZATIONS}' + '\\'
-snaphu_export_prdt = sp.snaphu_export(in_goldstein_prdt, snaphu_output_path)
-ProductIO.writeProduct(snaphu_export_prdt, snaphu_output_path, "Snaphu")
-logger.info("Snaphu Export write done")
+    unwrapped_phase_prdt = None
+    for file in os.listdir(snaphu_output_path):
+        if file.endswith('.hdr') and 'UnwPhase_ifg' in file:
+            unwrapped_phase_prdt = ProductIO.readProduct(snaphu_output_path + file)
+    if unwrapped_phase_prdt is None:
+        logger.critical(f'Unwrapped phase product not found in {snaphu_output_path} for phase unwrapping')
+        exit(1)
 
-### Unwrapping
-call_snaphu_command(snaphu_output_path)
+    # ### SNAPHU IMPORT
+    snaphu_import_prdt = sp.snaphu_import([unwrapped_phase_prdt, in_goldstein_prdt])
+    snaphu_import_path = output_dir + combined_name + f'_snaphu_import_{POLARIZATIONS}'
+    ProductIO.writeProduct(snaphu_import_prdt, snaphu_import_path, "BEAM-DIMAP")
 
-unwrapped_phase_prdt = None
-for file in os.listdir(snaphu_output_path):
-    if file.endswith('.hdr') and 'UnwPhase_ifg' in file:
-        unwrapped_phase_prdt = ProductIO.readProduct(snaphu_output_path + file)
-if unwrapped_phase_prdt is None:
-    logger.critical(f'Unwrapped phase product not found in {snaphu_output_path} for phase unwrapping')
-    exit(1)
+    # ### PHASE TO DISPLACEMENT
+    # snaphu_import_prdt = ProductIO.readProduct(snaphu_import_path + '.dim')
+    # ptd_prdt = sp.phase_to_disp(snaphu_import_prdt)
+    # ptd_path = output_dir + combined_name + f'_ptd_{polarizations}'
+    # ProductIO.writeProduct(ptd_prdt, ptd_path, "BEAM-DIMAP")
 
-# ### SNAPHU IMPORT
-snaphu_import_prdt = sp.snaphu_import([unwrapped_phase_prdt, in_goldstein_prdt])
-snaphu_import_path = output_dir + combined_name + f'_snaphu_import_{POLARIZATIONS}'
-ProductIO.writeProduct(snaphu_import_prdt, snaphu_import_path, "BEAM-DIMAP")
+    ### BandMaths
+    in_snaphu_import_prdt = ProductIO.readProduct(snaphu_import_path + '.dim')
+    unwrapped_phase_prdt_name = re.sub("\\..*$", "", unwrapped_phase_prdt.getName())
+    unwrapped_phase_prdt_name = unwrapped_phase_prdt_name.replace('UnwPhase', 'Unw_Phase')
+    unwrapped_phase_prdt_name = unwrapped_phase_prdt_name.replace('_VV', '')
+    vert_disp_prdt = sp.band_math(in_snaphu_import_prdt, 'vert_disp', f'({unwrapped_phase_prdt_name} * 0.056) / (-4 * PI * cos(rad(incident_angle)))')
+    # vert_disp_path = output_dir + combined_name + f'_vert_disp_subset_{polarizations}'
+    # ProductIO.writeProduct(vert_disp_prdt, vert_disp_path, "BEAM-DIMAP")
 
-# ### PHASE TO DISPLACEMENT
-# snaphu_import_prdt = ProductIO.readProduct(snaphu_import_path + '.dim')
-# ptd_prdt = sp.phase_to_disp(snaphu_import_prdt)
-# ptd_path = output_dir + combined_name + f'_ptd_{polarizations}'
-# ProductIO.writeProduct(ptd_prdt, ptd_path, "BEAM-DIMAP")
+    ### GEOCODING / TERRAIN CORRECTION
+    terrain_corrected_prdt = sp.terrain_correction(vert_disp_prdt, snappyconfigs.UTM_WGS84, 5.0)
 
-### BandMaths
-in_snaphu_import_prdt = ProductIO.readProduct(snaphu_import_path + '.dim')
-unwrapped_phase_prdt_name = re.sub("\\..*$", "", unwrapped_phase_prdt.getName())
-unwrapped_phase_prdt_name = unwrapped_phase_prdt_name.replace('UnwPhase', 'Unw_Phase')
-unwrapped_phase_prdt_name = unwrapped_phase_prdt_name.replace('_VV', '')
-vert_disp_prdt = sp.band_math(in_snaphu_import_prdt, 'vert_disp', f'({unwrapped_phase_prdt_name} * 0.056) / (-4 * PI * cos(rad(incident_angle)))')
-# vert_disp_path = output_dir + combined_name + f'_vert_disp_subset_{polarizations}'
-# ProductIO.writeProduct(vert_disp_prdt, vert_disp_path, "BEAM-DIMAP")
+    ### SUBSET
+    subset_prdt = sp.subset(terrain_corrected_prdt, LC_WKT) # TODO: replace wkt with cropped one, have to change to read wkt from file next time
+    subset_path = output_dir + combined_name + f'_vert_disp_subset_{POLARIZATIONS}'
+    ProductIO.writeProduct(subset_prdt, subset_path, "BEAM-DIMAP")
 
-### GEOCODING / TERRAIN CORRECTION
-terrain_corrected_prdt = sp.terrain_correction(vert_disp_prdt, snappyconfigs.UTM_WGS84, 5.0)
-
-### SUBSET
-subset_prdt = sp.subset(terrain_corrected_prdt, LC_WKT) # TODO: replace wkt with cropped one, have to change to read wkt from file next time
-subset_path = output_dir + combined_name + f'_vert_disp_subset_{POLARIZATIONS}'
-ProductIO.writeProduct(subset_prdt, subset_path, "BEAM-DIMAP")
+    count -= 1
+    if count == 0:
+        break
 
 logger.info("Completed")
 if usr_system == "Windows":
