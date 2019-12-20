6d289586242dee31d5cd339e4cb4d9bcff0bf474
jelneo <neohuilingjelena@gmail.com>
2019-12-20 15:46:08 +0800

    Fix plotting of confusion matrix
    Refactor code as well

--- sar_machine_learning/land_water_classification.py
+++ sar_machine_learning/land_water_classification.py
@@ -30,15 +30,15 @@ import seaborn as sns
 import logging
 import basicconfig as config
 from trainingdata import get_labelled_feature_as_df, extract_features_from
+import filemanager
+import platform
+import basicconfig as config
 
+input_dir, output_dir = filemanager.get_file_paths_based_on_os(platform.system(), filemanager.Product.grd)
+input_dir = output_dir + config.LC_PATH
 
-parent_dir = "E:\\GRD\\"
-input_dir = parent_dir + "Original\\"
-processing_dir = parent_dir + "Processing\\"
-processing_dir_LC = processing_dir + "LC\\"
-mask_dir = processing_dir + "LC_Mask\\"
-ml_dir = parent_dir + "ML\\LC\\"
-classified_LC_dir = processing_dir + "LC_classification\\"
+ml_dir = output_dir + config.ML_DIR
+classified_LC_dir = output_dir + config.LC_CLASSIFIED_DIR
 
 
 def convert_wkt_from_dd_to_m_to_polygon(wkt_in):
@@ -80,7 +80,7 @@ logger = logging.getLogger(__name__)
 
 
 # for folder in os.listdir(output_dir):
-def pretty_confusion_matrix(cm, title, class_names, normalize=False, figsize=(10, 7), fontsize=15):
+def pretty_confusion_matrix(cm, title, timestamp, class_names, normalize=False, figsize=(8, 6), fontsize=15):
     df_cm = pd.DataFrame(cm, index=class_names, columns=class_names)
     fig, ax = plt.subplots(figsize=figsize)
     try:
@@ -93,8 +93,10 @@ def pretty_confusion_matrix(cm, title, class_names, normalize=False, figsize=(10
     ax.xaxis.set_ticklabels(ax.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=fontsize)
     plt.ylabel('True label', fontsize=15)
     plt.xlabel('Predicted label', fontsize=15)
-    ttl = plt.title(f'Confusion Matrix for {title}', fontsize=18)
-    ttl.set_position([.5, 1.35])
+    ax.set(yticks=[-0.0, 2.0],
+           xticks=[0.5, 1.5],
+           )
+    ttl = plt.title(f'Confusion Matrix for {title}\n{timestamp}', fontsize=18, pad=20)
     return fig
 
 
@@ -111,13 +113,12 @@ def plot_feature_importance(feature_names, importances):
 
 # Get dataset
 # TODO: currently works for one prdt, loop over directories to classify all prdts
-loop_dir = processing_dir_LC
+loop_dir = input_dir
 for folder in os.listdir(loop_dir):
     # acc_list = []
     # feat_impt_list = []
     logger.info(folder)
-    if folder.endswith('.data') and f'glcm_{config.POLARIZATIONS}' in folder and ('S1B_IW_GRDH_1SDV_20190101T111959_20190101T112024_014300_01A9AA_8AF3_glcm_VV' in folder \
-                                                                                  or "S1A_IW_GRDH_1SDV_20190107T112034_20190107T112059_025371_02CF15_BACB_glcm_VV" in folder):
+    if folder.endswith('.data') and f'glcm_{config.POLARIZATIONS}' in folder:
         # for a in range(100):
             # feature_df = get_labelled_data(loop_dir + '\\' + folder)
             feature_df = get_labelled_feature_as_df(loop_dir + folder)
@@ -158,8 +159,9 @@ for folder in os.listdir(loop_dir):
             # accuracy = accuracy_score(test_y, test_y_pred)
             # acc_list.append(accuracy)
             confusion_mtx = confusion_matrix(test_y, test_y_pred)
-            c_mtx = pretty_confusion_matrix(confusion_mtx, "Land-water classification", ['Land', 'Water'])
-            c_mtx.savefig(parent_dir + "cm.jpg")
+            timestamp = folder.split("_")[4]
+            c_mtx = pretty_confusion_matrix(confusion_mtx, "Land-water classification", timestamp, ['Land', 'Water'])
+            c_mtx.savefig(ml_dir + "cm.jpg")
             c_mtx.show()
             print(classification_report(test_y, test_y_pred))
 
@@ -168,7 +170,7 @@ for folder in os.listdir(loop_dir):
 
             logger.info("Classifying whole image...")
             prdt = rasterio.open(loop_dir + '\\' + folder + '\\' + f'Sigma0_{config.POLARIZATIONS}_db_GLCMMean.img')
-            to_predict = extract_features_from(processing_dir_LC + folder)
+            to_predict = extract_features_from(input_dir + folder)
             to_predict_scaled = scaler.transform(to_predict)
             predicted_flatten = clf.predict(to_predict_scaled)
             logger.info("Done predicting")
@@ -180,7 +182,6 @@ for folder in os.listdir(loop_dir):
             img = Image.fromarray(predicted)
             img.show()
 
-            timestamp = folder.split("_")[4]
             with rasterio.open(
                     classified_LC_dir + f"land_water_mask_rf_{timestamp}_{config.POLARIZATIONS}.tif",
                     'w',
