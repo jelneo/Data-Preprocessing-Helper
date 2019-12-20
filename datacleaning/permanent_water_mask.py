e487b0feadf4c1d87ff09e6bbcb282ff63171b03
jelneo <neohuilingjelena@gmail.com>
2019-12-20 17:35:25 +0800

    Rename masks

--- datacleaning/permanent_water_mask.py
+++ datacleaning/permanent_water_mask.py
@@ -62,64 +62,64 @@ area_threshold = 0.96 * sample_spacing * sample_spacing
 mask_json = {}
 loop_dir = classified_LC_dir
 for folder in os.listdir(loop_dir):
-    if folder.endswith(".tif") and f'land_water_mask_rf' in folder:
-        logger.info(folder)
-        img = rasterio.open(loop_dir + folder)
-        msk = Image.open(loop_dir + folder)
-        width = img.width
-        height = img.height
-        binary_img = img.read(1)
-        # print(binary_img)
-        logger.info(binary_img.shape)
-        # print(binary_img[162][41])#0
-        # print(binary_img[41][162])
-        done = False
-        # To reduce unnecessary checking, we starting search for the reservoir at an arbitrary point
-        for x in range(0, height, sample_spacing // 2):
-            for y in range(0, width, sample_spacing // 2):
-                if binary_img[x][y] == BLACK:
-                    if is_area_larger_than(binary_img, x, y, sample_spacing, area_threshold) and is_area_larger_than(binary_img, x + sample_spacing // 2, y, sample_spacing, area_threshold):
-                        logger.info("Found the reservoir!")
-                        ImageDraw.floodfill(msk, (y, x), DAM_COLOR)
-                        done = True
-                        break
-            if done:
-                break
-        if not done:
-            logger.critical("No reservoir was found.")
-            sys.exit("No reservoir was found.")
-        msk_arr = np.array(msk).astype(np.uint8)
-        msk_arr[msk_arr == WHITE] = BLACK
-        msk_arr = ndimage.binary_fill_holes(msk_arr).astype(np.uint8)
-        msk_arr[msk_arr == 1] = DAM_COLOR
-        # msk = Image.fromarray(msk_arr)
-        # msk.show()
+    logger.info(folder)
+    img = rasterio.open(loop_dir + folder)
+    msk = Image.open(loop_dir + folder)
+    width = img.width
+    height = img.height
+    binary_img = img.read(1)
+    # print(binary_img)
+    logger.info(binary_img.shape)
+    # print(binary_img[162][41])#0
+    # print(binary_img[41][162])
+    done = False
+    # To reduce unnecessary checking, we starting search for the reservoir at an arbitrary point
+    for x in range(0, height, sample_spacing // 2):
+        for y in range(0, width, sample_spacing // 2):
+            if binary_img[x][y] == BLACK:
+                if is_area_larger_than(binary_img, x, y, sample_spacing, area_threshold) and is_area_larger_than(binary_img, x + sample_spacing // 2, y, sample_spacing, area_threshold):
+                    logger.info("Found the reservoir!")
+                    ImageDraw.floodfill(msk, (y, x), DAM_COLOR)
+                    done = True
+                    break
+        if done:
+            break
+    if not done:
+        logger.critical("No reservoir was found.")
+        sys.exit("No reservoir was found.")
+    msk_arr = np.array(msk).astype(np.uint8)
+    msk_arr[msk_arr == WHITE] = BLACK
+    msk_arr = ndimage.binary_fill_holes(msk_arr).astype(np.uint8)
+    msk_arr[msk_arr == 1] = DAM_COLOR
+    # msk = Image.fromarray(msk_arr)
+    # msk.show()
 
-        file_name = re.sub("\\..*$", "", folder)
-        mask_prdt_path = mask_dir + file_name + '.tif'
-        with rasterio.open(
-            mask_prdt_path,
-            'w',
-            driver='GTiff',
-            height=img.height,
-            width=img.width,
-            count=1,
-            dtype=np.uint8,
-            nodata=BLACK,
-            crs=img.crs,
-            transform=img.transform,
-        ) as dst:
-            dst.write(msk_arr, 1)
+    file_name = re.sub("\\..*$", "", folder)
+    date = file_name.split('_')[-2][:8]
+    mask_prdt_path = mask_dir + date + f'_{POLARIZATIONS}' + '.tif'
+    with rasterio.open(
+        mask_prdt_path,
+        'w',
+        driver='GTiff',
+        height=img.height,
+        width=img.width,
+        count=1,
+        dtype=np.uint8,
+        nodata=BLACK,
+        crs=img.crs,
+        transform=img.transform,
+    ) as dst:
+        dst.write(msk_arr, 1)
 
-        mask = None
-        with rasterio.open(mask_prdt_path, 'r') as src:
-            image = src.read(1, masked=True)  # first band
-            results = (
-                {'properties': {'raster_val': v}, 'geometry': s}
-                for i, (s, v) in enumerate(shapes(image, mask=mask, transform=src.transform)))
-        geoms = list(results)
-        shp = shape(geoms[0]['geometry'])
-        mask_json[file_name] = str(shp)
+    mask = None
+    with rasterio.open(mask_prdt_path, 'r') as src:
+        image = src.read(1, masked=True)  # first band
+        results = (
+            {'properties': {'raster_val': v}, 'geometry': s}
+            for i, (s, v) in enumerate(shapes(image, mask=mask, transform=src.transform)))
+    geoms = list(results)
+    shp = shape(geoms[0]['geometry'])
+    mask_json[date + f'_{POLARIZATIONS}'] = str(shp)
 with open(mask_dir + 'data.json', 'w', encoding='utf-8') as f:
     json.dump(mask_json, f, ensure_ascii=False, indent=4)
 logger.info("Completed")
