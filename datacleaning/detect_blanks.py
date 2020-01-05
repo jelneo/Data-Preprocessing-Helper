49f825f45534ccd2e067fdef390a6a162d59db0a
jelneo <neohuilingjelena@gmail.com>
2020-01-05 18:31:16 +0800

    Add detection of blanks for .dim products

--- datacleaning/detect_blanks.py
+++ datacleaning/detect_blanks.py
@@ -2,7 +2,8 @@ import os
 import rasterio
 import numpy as np
 
-parent_dir = "E:\\GRD\\"
+parent_dir = "D:\\FYP_IW\\"
+# parent_dir = "E:\\GRD\\"
 input_dir = parent_dir + "Original\\"
 output_dir = parent_dir + "Processing\\"
 
@@ -11,6 +12,14 @@ for folder in os.listdir(output_dir):
     if folder.endswith('.tif'):
         print("Current folder: " + folder)
         data = rasterio.open(output_dir + folder)
+        data_arr = data.read(1)
+        std_dev = np.std(data_arr)
+        if std_dev == 0:
+            f.write(folder[:50] + "\n")
+    elif folder.endswith('.data') and "top_sar_split" in folder:
+        file = output_dir + folder + "\\q_IW2_VV.img"
+        print("Current folder: " + file)
+        data = rasterio.open(file)
         data_arr = data.read(1)
         std_dev = np.std(data_arr)
         if std_dev == 0:
